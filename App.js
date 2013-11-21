88203a020159736ab3604ff84962c7418869645f
Mike McLaughlin <mike_atm_97@yahoo.com>
2013-11-21 11:28:28 -0700

    Put chart it its own container

--- App.js
+++ App.js
@@ -9,15 +9,21 @@ Ext.define('CustomApp', {
     items:[
 
         { // define a container to house header info about the PI
-            xtype:'container',
-            itemId:'appHeaderContainer',
-            padding:'15 15 15 15' // top ? bottom left,
+            xtype: 'container',
+            itemId: 'appHeaderContainer',
+            padding: '15 15 15 15' // top ? bottom left,
         },
 
         { // define a container to house the grid of features
-            xtype:'container',
-            itemId:'featureGridContainer',
-            padding:'15 15 15 15' // top ? bottom left,
+            xtype: 'container',
+            itemId: 'featureGridContainer',
+            padding: '15 15 15 15' // top ? bottom left,
+        },
+
+        { // define a container to house the CFD diagram
+            xtype: 'container',
+            itemId: 'cfdContainer',
+            padding: '15 15 15 15' // top ? bottom left,
         }
     ],
 
@@ -29,12 +35,14 @@ Ext.define('CustomApp', {
 
     gFeatureObjectIDArr: [],    // Array of the object ID's of the features selected
 
+    aCalc: undefined,
+
     // --- end global variables ---
 
 
     launch: function() {
         //Write app code here
-        console.log("App in Running!!!")
+        console.log("App in Running!!!");
 
 
         if (this.gSkipTagPicker === false)
@@ -134,6 +142,8 @@ Ext.define('CustomApp', {
 
         this._gridTaggedFeatures(theTaggedFeatures); // throw the tagged features into a grid
 
+        this._driveCFD(); // drive the CFD chart for the tagged features
+
 
         console.log("E ==> _tableOfContents()" + "\n");
 
@@ -155,11 +165,11 @@ Ext.define('CustomApp', {
 
         Ext.each(theTaggedFeatures, function(aFeature){
 
-            if (aFeature != null) {
+            if (aFeature !== null) {
 
                 var aFeatureObjectID = aFeature.get("ObjectID");
                 var aFeatureName = aFeature.get("Name");
-                var aProjectName = aFeature.get("Project")["Name"];
+                var aProjectName = aFeature.get("Project").Name;
 
                 console.log("aFeatureObjectID: " + aFeatureObjectID);
                 console.log("aFeatureName: " + aFeatureName);
@@ -223,6 +233,146 @@ Ext.define('CustomApp', {
 
         console.log("E ==> _gridTaggedFeatures()" + "\n");
 
-    } // end gridTaggedFeatures()
+    }, // end gridTaggedFeatures()
+
+
+    // ----------------------------------------------------
+    // ----------------------------------------------------
+    // ----- Call the CFD prototype code that I found
+    // ----------------------------------------------------
+    // ----------------------------------------------------
+    _driveCFD: function() {
+
+        console.log("B ==> _driveCFD()");
+
+
+        var TIME_PERIOD_IN_MONTHS = 2,
+            TIME_PERIOD_IN_MILLIS = 1000 * 60 * 60 * 24 * 30 * TIME_PERIOD_IN_MONTHS;
+
+
+        this.aCalc = Ext.define("ProjectCFDCalculator", {
+            extend: "Rally.data.lookback.calculator.TimeSeriesCalculator",
+            values: ["Idea","Defined","In-Progress","Completed","Accepted","Released"],
+
+            getDerivedFieldsOnInput : function () {
+                var dfs = [];
+                var fieldName = 'ScheduleState';
+                var that = this;
+                _.each(that.values,function(value) {
+
+                    var s = "return snapshot['"+fieldName+"'] == '" + value + "' ? 1 : 0;";
+
+                    var fn = new Function("snapshot",s);
+                    dfs.push({
+                        as : value,
+                        f : fn
+                    });
+                });
+                return dfs;
+            }
+            ,
+            getMetrics: function() {
+                var metrics = [];
+                var that = this;
+                _.each(that.values,function(value) {
+                    metrics.push({
+                        field : value,
+                        as : value,
+                        f : "sum",
+                        display : "area"
+                    });
+                });
+                console.log(metrics);
+                return metrics;
+            }
+        }); // end define the calculator
+
+
+
+
+        // --- drive the CFD stuff ---
+
+        var today = new Date();
+        var timePeriod = new Date(today - TIME_PERIOD_IN_MILLIS);
+
+
+        // ----- Hard-code in M^2 Enterprises -----
+        //this.chartConfig.storeConfig.find['_ProjectHierarchy'] = this.getContext().getProject().ObjectID;
+        //this.chartConfig.storeConfig.find['Project'] = {"$exists":true};
+        this.chartConfig.storeConfig.find['_ProjectHierarchy'] = 11377984352;
+        this.chartConfig.storeConfig.find['Project'] = {"$exists":true};
+
+
+        this.chartConfig.storeConfig.find['_ValidFrom'] = {
+            "$gt": timePeriod.toISOString()
+        };
+        this.chartConfig.chartConfig.title = {
+            text: this.getContext().getProject().Name + " Cumulative Flow Diagram"
+        };
+
+
+        // experiment with how we add the CFD to the page
+        //this.add(this.chartConfig);
+
+        var cfdHolder = this.down('#cfdContainer');
+        cfdHolder.removeAll(true);
+        cfdHolder.add(this.chartConfig);
+
+
+
+        console.log("B ==> _driveCFD()");
+
+    }, // end _driveCFD()
+
+
+    chartConfig: {
+        xtype: 'rallychart',
+
+        storeConfig: {
+            find : {
+                '_TypeHierarchy': { '$in' : ['HierarchicalRequirement','Defect']},
+                'Children': null
+            },
+            fetch: ['ScheduleState', 'PlanEstimate'],
+            hydrate: ["ScheduleState"]
+        },
+
+        calculatorType: 'ProjectCFDCalculator',
+        calculatorConfig: {
+        },
+
+        chartConfig: {
+            chart: {
+                zoomType: 'xy'
+            },
+            title: {
+                text: 'Cumulative Flow Diagram'
+            },
+            xAxis: {
+                tickmarkPlacement: 'on',
+                tickInterval: 20,
+                title: {
+                    text: 'Days'
+                }
+            },
+            yAxis: [
+                {
+                    title: {
+                        text: 'Count'
+                    }
+                }
+            ],
+            plotOptions: {
+                series: {
+                    marker: {
+                        enabled: true
+                    }
+                },
+                area: {
+                    stacking: 'normal'
+                }
+            }
+        } // end chartConfig (inner)
+    } // end chartConfig (outer)
 
 });
